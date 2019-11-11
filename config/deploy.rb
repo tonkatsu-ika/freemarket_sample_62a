3446dbf2827b83031c2169b9bbb849b7db9c2993
yoshipjr <yoshihisa441396@gmail.com>
2019-11-11 16:45:04 +0900

    fix_deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,4 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.2"
-
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
