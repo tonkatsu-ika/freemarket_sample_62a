e1375734fb693ebd810f43ea58e88d624f93cc21
yoshipjr <yoshihisa441396@gmail.com>
2019-11-12 19:24:28 +0900

    create user login
    ユーザーログインの新規作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  :layout_by_resource
+  
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  
+
+  protected
+
+  def layout_by_resource
+    if devise_controller?
+      "user"
+    else
+      "application"
+    end
+  end
 end
