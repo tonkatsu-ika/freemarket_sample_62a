dbe12555b684ebe74f535c4fba0f10dda7c030db
yoshipjr <yoshihisa441396@gmail.com>
2019-11-14 12:37:33 +0900

    create addresses miglation

---
+++ db/migrate/20191114032751_create_addresses.rb
@@ -0,0 +1,12 @@
+class CreateAddresses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :addresses do |t|
+      t.timestamps
+      t.integer	    :post_code,	     null: false
+      t.string	    :prefecuture,	   null: false
+      t.string	    :address,	       null: false
+      t.string	    :building,	     null: false
+      t.references	:user,	         foreign_key: true
+    end
+  end
+end
