aa9ba123e316f3ebb1e2503b149d9dc731c986c8
naoki <duvingtdeux@gmail.com>
2019-11-14 15:34:49 +0900

    サイドメニューのアニメーション作成

--- app/assets/javascripts/mypage/sidebar.js
+++ app/assets/javascripts/mypage/sidebar.js
@@ -3,10 +3,11 @@ $(document).on('turbolinks:load', function() {
   $('.mypage-sidebar__list__item').hover(function() {
     console.log('OK');
     console.log(this);
-    $(this).addClass('hover');
+    $(this).addClass('mypage-sidebar__list__item--hover');
   }, function() {
     console.log('hover-out');
-    $(this).removeClass('hover');
+    $(this).removeClass('mypage-sidebar__list__item--hover');
+    $(this).addClass('mypage-sidebar__list__item');
   });
 
 
