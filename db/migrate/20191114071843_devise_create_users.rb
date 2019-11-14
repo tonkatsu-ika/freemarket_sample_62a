edfc6708e288f8d4134816f831841f9eb698d33a
yoshipjr <yoshihisa441396@gmail.com>
2019-11-14 16:55:32 +0900

    マイグレーションファイルの修正
    マイグレーションファイルの修正

--- db/migrate/20191114071843_devise_create_users.rb
+++ db/migrate/20191114071843_devise_create_users.rb
@@ -6,16 +6,15 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string  :last_name,          null: false
-      t.string  :first_name,         null: false
-      t.string  :last_name_kana,     null: false
-      t.string  :first_name_kana,    null: false
-      t.string  :nickname,           null: false
-      t.string  :profile_image
-      t.text    :profile,            null: false
-      t.date    :birthday,           null: false
-      t.integer :telephone,          null: false
-
+      t.string  :last_name,          null: false
+      t.string  :first_name,         null: false
+      t.string  :last_name_kana,     null: false
+      t.string  :first_name_kana,    null: false
+      t.string  :nickname,           null: false
+      t.string  :profile_image
+      t.text    :profile,            null: false
+      t.date    :birthday,           null: false
+      t.integer :telephone,          null: false
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
