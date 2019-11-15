38c596f823425830f5b660423599ed1b088dfaea
yoshipjr <yoshihisa441396@gmail.com>
2019-11-15 14:07:15 +0900

    gem active_hashの追加　prefecutreテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          has_one :address, dependent: :destroy       
+         belongs_to_active_hash :prefecture
 end
