50c44b2e61626858ab1efb21588253cc76f408e6
naoki <duvingtdeux@gmail.com>
2019-11-15 09:51:16 +0900

    ブランドスライドメニューのスタイル作成

--- app/assets/javascripts/layouts/slide_menu.js
+++ app/assets/javascripts/layouts/slide_menu.js
@@ -1,4 +1,11 @@
+// ヘッダーのブランドスライドメニュー
 $(document).on('turbolinks:load', function () {
 
-  
+  $('#slide_brands').mouseover(function() {
+    console.log('in');
+  });
+  $('#slide_brands').mouseout(function() {
+    console.log('out');
+  });
+
 });
