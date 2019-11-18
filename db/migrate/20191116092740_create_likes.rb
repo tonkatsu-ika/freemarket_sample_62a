6be03d54caad5dc334fd335623b00a95d512d0a4
moroball14 <daiki.mr.0330@gmail.com>
2019-11-18 11:36:02 +0900

    t.bigintへ変更

--- db/migrate/20191116092740_create_likes.rb
+++ db/migrate/20191116092740_create_likes.rb
@@ -1,7 +1,7 @@
 class CreateLikes < ActiveRecord::Migration[5.2]
   def change
     create_table :likes do |t|
-      t.references :user, foreign_key: true, type: :bigint
+      t.bigint :user, foreign_key: true
       t.references :item, foreign_key: true
 
       t.timestamps
