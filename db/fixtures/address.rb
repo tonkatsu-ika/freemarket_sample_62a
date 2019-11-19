a9d179bc31bf7bdd16d96597b60c23b918844dd4
naoki <duvingtdeux@gmail.com>
2019-11-19 21:30:35 +0900

    addressデータを作成

---
+++ db/fixtures/address.rb
@@ -0,0 +1,11 @@
+require 'faker'
+
+5.times do |i|
+  Address.seed_once do |s|
+    s.post_code = Faker::Address.zip_code
+    s.prefecture = Prefecture.find(i+1).name
+    s.address = "#{Faker::Address.city} #{Faker::Address.street_name} #{Faker::Address.building_number}"
+    s.building = "#{Faker::Name.last_name}ビル"
+    s.user_id = i + 1
+  end
+end
