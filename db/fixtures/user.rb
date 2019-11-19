823c3c4ea54eefb3c9f81e45309068029282d6b6
naoki <duvingtdeux@gmail.com>
2019-11-19 21:04:52 +0900

    userのデータ作成

--- db/fixtures/user.rb
+++ db/fixtures/user.rb
@@ -7,7 +7,17 @@ csv = CSV.read(csv_path)
 csv.each do |data|
   User.seed_once do |s|
     s.id = data[0]
-    s.last_name = 
+    s.last_name = Faker::Name.last_name
+    s.first_name = Faker::Name.first_name
+    s.last_name_kana = Faker::Name.last_name.tr("あ-ん", "ア-ン")
+    s.first_name_kana = Faker::Name.first_name.tr("あ-ん", "ア-ン")
+    s.nickname = data[1]
+    s.profile_image = Faker::Avatar.image(size: "60x60", format: "jpg")
+    s.profile = data[2]
+    s.email = "test#{data[0]}@example.com"
+    s.password = "password"
+    s.birthday = Faker::Date.birthday(min_age: 18, max_age: 65)
+    s.telephone = Faker::PhoneNumber.phone_number
   end
 end
 
