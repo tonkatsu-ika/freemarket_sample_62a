44987640c326eb0e00f0d194b65aa5aa4de4c89b
naoki <duvingtdeux@gmail.com>
2019-11-19 09:30:20 +0900

    seed data 作成

--- db/fixtures/days_before_ship.rb
+++ db/fixtures/days_before_ship.rb
@@ -1,14 +1,14 @@
-DaysBeforeShip.seed do |s|
+DaysBeforeShip.seed_once do |s|
   s.id = 1
   s.days_before_ship = "1~2日で発送"
 end
 
-DaysBeforeShip.seed do |s|
+DaysBeforeShip.seed_once do |s|
   s.id = 2
   s.days_before_ship = "2~3日で発送"
 end
 
-DaysBeforeShip.seed do |s|
+DaysBeforeShip.seed_once do |s|
   s.id = 3
   s.days_before_ship = "4~7日で発送"
 end
