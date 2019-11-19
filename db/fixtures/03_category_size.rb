80153b544edceec49f381aa980c14d6a76558682
naoki <duvingtdeux@gmail.com>
2019-11-19 19:31:28 +0900

    category_sizeデータの修正

--- db/fixtures/03_category_size.rb
+++ db/fixtures/03_category_size.rb
@@ -6,7 +6,7 @@ csv = CSV.read(csv_path)
 csv.each do |data|
   CategorySize.seed_once do |s|
     s.id = data[0]
-    s.category_id = data[1]
-    s.size_id = data[2]
+    s.category = data[1]
+    s.size = data[2]
   end
 end
