722a63a4c4b2435e3285ab4ca553d9de00396199
naoki <duvingtdeux@gmail.com>
2019-11-20 12:01:09 +0900

    item imagesを作成

---
+++ db/fixtures/item_image.rb
@@ -0,0 +1,13 @@
+require 'csv'
+
+image_dir = "app/assets/images/item_images/"
+csv_path = "db/fixtures/csv/item_images.csv"
+csv = CSV.read(csv_path)
+
+csv.each do |data|
+  ItemImage.seed_once do |s|
+    s.id = data[0]
+    s.image_url = "#{image_dir}#{data[1]}"
+    s.item_id = data[2]
+  end
+end
