a9b84883a7c0c689aec528ceb8c0df00df0094e3
naoki <duvingtdeux@gmail.com>
2019-11-20 14:07:15 +0900

    category, category_size, sizeのアソシエーションを修正

--- app/models/size.rb
+++ app/models/size.rb
@@ -1,3 +1,4 @@
 class Size < ApplicationRecord
+  has_many :categories, through: :category_users
   has_many :category_sizes
 end
--- db/schema.rb
+++ db/schema.rb
@@ -193,13 +193,13 @@ ActiveRecord::Schema.define(version: 2019_11_18_060053) do
     t.string "nickname", null: false
     t.string "profile_image"
     t.date "birthday", null: false
-    t.string "telephone"
+    t.string "telephone", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "profile"
+    t.text "profile", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
