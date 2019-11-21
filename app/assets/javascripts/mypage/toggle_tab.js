4ba379d3e1284edeef8af9b4caac48fa4938cba0
naoki <duvingtdeux@gmail.com>
2019-11-21 16:57:45 +0900

    タブ切り替えの完成

--- app/assets/javascripts/mypage/toggle_tab.js
+++ app/assets/javascripts/mypage/toggle_tab.js
@@ -3,16 +3,33 @@ $(document).on('turbolinks:load', function() {
   // タブの切替
   $('.index-contents__tab-container__tabs__tab__link').click(function(e) {
     e.preventDefault();
-    console.log(this);
     let clickedTab = $(this).data('tab');
-    console.log(clickedTab);
+
+
     if (clickedTab === 'notice' || clickedTab === 'todo') {
+      //上部のタブコンテナ
+      
+      //タブの切替
+      let notClickedTab = clickedTab === 'notice' ? 'todo' : 'notice';
+      $('a[data-tab=' + notClickedTab + ']').removeClass('active');
+      $(this).addClass('active');
+
+      //コンテンツの切替
       $('.index-contents__tab-container__items').removeClass('active');
-      $('.index-contents__tab-container__items').data(clickedTab)
+      $('ul[data-items=' + clickedTab + ']').addClass('active');
 
     } else if (clickedTab === 'transaction-now' || clickedTab === 'transaction-old') {
-      console.log('clickd transactions');
+      //下部のタブ
+
+      //タブの切替
+      let notClickedTab = clickedTab === 'transaction-now' ? 'transaction-old' : 'transaction-now';
+      $('a[data-tab=' + notClickedTab + ']').removeClass('active');
+      $(this).addClass('active');
+
+      //コンテンツの切替
       $('.index-contents__tab-container__contents__transactions').removeClass('active');
+      $('ul[data-items=' + clickedTab + ']').addClass('active');
+
     }
 
   });
