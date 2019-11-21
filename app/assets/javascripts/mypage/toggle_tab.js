512cbabc46c8203be1b9cb793a971958e45e14d7
naoki <duvingtdeux@gmail.com>
2019-11-21 15:19:34 +0900

    タブをクリックするとアイテムを非表示にするところまで作成

--- app/assets/javascripts/mypage/toggle_tab.js
+++ app/assets/javascripts/mypage/toggle_tab.js
@@ -1,9 +1,20 @@
 $(document).on('turbolinks:load', function() {
 
-
+  // タブの切替
   $('.index-contents__tab-container__tabs__tab__link').click(function(e) {
     e.preventDefault();
-    
+    console.log(this);
+    let clickedTab = $(this).data('tab');
+    console.log(clickedTab);
+    if (clickedTab === 'notice' || clickedTab === 'todo') {
+      $('.index-contents__tab-container__items').removeClass('active');
+      $('.index-contents__tab-container__items').data(clickedTab)
+
+    } else if (clickedTab === 'transaction-now' || clickedTab === 'transaction-old') {
+      console.log('clickd transactions');
+      $('.index-contents__tab-container__contents__transactions').removeClass('active');
+    }
+
   });
 
 });
