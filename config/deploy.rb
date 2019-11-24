5d24b04d23fe55c008f5a74826a6105f2bc36916
naoki <duvingtdeux@gmail.com>
2019-11-24 19:40:52 +0900

    deploy.rbでunicornの起動コマンドを変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -68,6 +68,7 @@ set :keep_releases, 5
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
-    invoke 'unicorn:restart'
+    #invoke 'unicorn:restart'
+    invoke 'unicorn:reload'
   end
 end
