b7753dab98e2f5dc5d59388b08e2760f684a5e93
naoki <duvingtdeux@gmail.com>
2019-11-25 11:23:44 +0900

    categoryのシードを重複して作成しないようにロジック変更

--- db/fixtures/01_category_closure_tree.rb
+++ db/fixtures/01_category_closure_tree.rb
@@ -6,13 +6,17 @@ category_csv = CSV.read(category_csv_path)
 
 records = []
 
-category_csv.each_with_index do |data, i|
-  if data[2] == nil
-    records << Category.create(category_name: data[1])
-  else
-    child = Category.create(category_name: data[1])
-    records << child
-    Category.find(data[2]).add_child child
+
+# カテゴリデータがない場合のみseedを作る
+if Category.count = 0
+  category_csv.each_with_index do |data, i|
+    if data[2] == nil
+      records << Category.create(category_name: data[1])
+    else
+      child = Category.create(category_name: data[1])
+      records << child
+      Category.find(data[2]).add_child child
+    end
+    p records[i]
   end
-  p records[i]
 end
