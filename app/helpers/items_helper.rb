e46f02075548d406928ab6515ef844e4b0fe8251
naoki <duvingtdeux@gmail.com>
2019-11-25 17:48:26 +0900

    販売手数料と販売利益計算のためのヘルパーメソッド作成

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,14 @@
 module ItemsHelper
+
+  COMMISSION_RATE = 0.1
+  
+  # 販売手数料の計算
+  def calc_fm_commission(price)
+    return (price * COMMISSION_RATE).floor.to_i
+  end
+
+  # 販売利益の計算
+  def calc_fm_profit(price)
+    return price - calc_fm_commission(price)
+  end
 end
