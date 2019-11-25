1fdf238256324140acc520bd84843d8cb7b6f48b
tomohiro1105 <paripari500@icloud.com>
2019-11-25 12:09:30 +0900

    ビューファイル修正

--- db/schema.rb
+++ db/schema.rb
@@ -203,9 +203,9 @@ ActiveRecord::Schema.define(version: 2019_11_23_083532) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "profile"
     t.string "provider"
     t.string "uid"
+    t.text "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
