539d9dedcef807786df7db8b45ec113619f89c3d
moroball14 <daiki.mr.0330@gmail.com>
2019-11-26 13:44:04 +0900

    if文を一行で記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -48,9 +48,7 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    if (current_user.id == item.user_id)
-      item.destroy
-    end
+    item.destroy if current_user.id == item.user_id
   end
 
   def show
