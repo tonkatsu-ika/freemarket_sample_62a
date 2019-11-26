645dd009cd6778e4adb5fa8de5e9fd0b565bbeaa
yutaroyoshida <yoshida19950501@gmail.com>
2019-11-26 19:17:54 +0900

    add_slide_to_toppage
    トップページにスライドを追加。

--- app/assets/javascripts/items/index.js
+++ app/assets/javascripts/items/index.js
@@ -1,12 +1,7 @@
 $(function(){
   $("#index-slide").slick({
     speed: 1000,
-    dots: true,
-    // prevArrow: '<button class="fas.fa-chevron-left"></button>',
-    // nextArrow: '<button class="fas.fa-chevron-right"></button>'
-    nextArrow: '<i class="fas fa-chevron-right"></i>',
-    prevArrow: '<i class="fas fa-chevron-left"></i>',
-    
-
+    dots: false,
+    autoplay: 500,
 });
 });
