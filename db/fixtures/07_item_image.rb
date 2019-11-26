0efaf77c542e2f1a1c29f5317d1ee4ee114ddd5b
naoki <duvingtdeux@gmail.com>
2019-11-26 19:47:27 +0900

    db:seeding item_imagesのシードのエラー解消

--- db/fixtures/07_item_image.rb
+++ db/fixtures/07_item_image.rb
@@ -1,13 +1,14 @@
 require 'csv'
 
-image_dir = "public/uploads/item_image/image_url"
+image_dir = "app/assets/images/item_images"
 csv_path = "db/fixtures/csv/item_images.csv"
 csv = CSV.read(csv_path)
 
 csv.each do |data|
   ItemImage.seed_once do |s|
     s.id = data[0]
-    s.image_url = "#{image_dir}/#{data[0]}/#{data[1]}"
+    s.image_url = Rails.root.join("#{image_dir}/#{data[1]}").open
+    #s.image_url = "#{image_dir}/#{data[0]}/#{data[1]}"
     s.item_id = Item.find(data[2]).id
   end
 end
