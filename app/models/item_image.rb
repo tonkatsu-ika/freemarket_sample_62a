3e58be359256ac88342dfc56dd7c654283bb664d
moroball14 <daiki.mr.0330@gmail.com>
2019-11-27 16:49:06 +0900

    商品出品周りを修正

--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -2,7 +2,7 @@ class ItemImage < ApplicationRecord
   belongs_to :item, optional: true
   mount_uploader :image_url, ImageUploader
 
-  # validates :image_url, presence: true
+  validates :image_url, presence: true
   # validate :image_url_validation #image_urlが1〜10枚であること
 
   # def image_url_validation
