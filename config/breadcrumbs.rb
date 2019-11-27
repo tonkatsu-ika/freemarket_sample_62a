8bed7a0c5ab7780e369c426f6e3040d59ac61a82
moroball14 <daiki.mr.0330@gmail.com>
2019-11-27 10:39:43 +0900

    マイページのパンくず作成とルーティング修正

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -7,6 +7,28 @@ crumb :mypage do
   parent :root
 end
 
+crumb :profile do
+  link "プロフィール", profile_mypage_index_path
+  parent :mypage
+end
+
+crumb :card do
+  link "支払い方法", card_mypage_index_path
+  parent :mypage
+end
+
+crumb :identification do
+  link "本人情報の登録", identification_mypage_index_path
+  parent :mypage
+end
+
+crumb :logout do
+  link "ログアウト", logout_mypage_index_path
+  parent :mypage
+end
+
+
+
 # crumb :projects do
 #   link "Projects", projects_path
 # end
