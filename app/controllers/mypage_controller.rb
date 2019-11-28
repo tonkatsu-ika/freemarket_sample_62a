3428ac11d4b2a8bdcf6c32dbe3221355ed893fe8
moroball14 <daiki.mr.0330@gmail.com>
2019-11-28 12:42:15 +0900

    クレジットカード情報を表示

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -9,6 +9,30 @@ class MypageController < ApplicationController
   end
 
   def card
+    @card = CreditCard.where(user_id: current_user.id).first if CreditCard.where(user_id: current_user.id).present?
+    if @card.present?
+      Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @card_information = customer.cards.retrieve(@card.card_id)
+
+      # 《＋α》 登録しているカード会社のブランドアイコンを表示するためのコードです。---------
+      @card_brand = @card_information.brand
+      case @card_brand
+      when "Visa"
+        @card_src = "visa.svg"
+      when "JCB"
+        @card_src = "jcb.gif"
+      when "MasterCard"
+        @card_src = "mastercard.svg"
+      when "American Express"
+        @card_src = "amex.gif"
+      when "Diners Club"
+        @card_src = "diners.gif"
+      when "Discover"
+        @card_src = "discover.gif"
+      end
+      # ---------------------------------------------------------------
+    end
   end
 
   def identification
