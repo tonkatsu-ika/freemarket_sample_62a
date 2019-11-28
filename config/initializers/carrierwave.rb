233c4d2025d034fdaa745c056051a2c85ba5a21c
naoki <duvingtdeux@gmail.com>
2019-11-28 21:52:23 +0900

    fix syntax errors

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,8 +9,8 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     #aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     #aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"]
-    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
+    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
     region: 'ap-northeast-1'
   }
 
