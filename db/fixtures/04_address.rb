863dafb330d27d1ae7817ff3d884cfeec9e73c15
naoki <duvingtdeux@gmail.com>
2019-11-29 11:17:01 +0900

    address pref pref idに変更

--- db/fixtures/04_address.rb
+++ db/fixtures/04_address.rb
@@ -4,7 +4,7 @@ require 'faker'
   Address.seed_once do |s|
     s.id = i + 1
     s.post_code = Faker::Address.zip_code
-    s.prefecture = i + 1
+    s.prefecture_id = i + 1
     s.address = "#{Faker::Address.city} #{Faker::Address.street_name} #{Faker::Address.building_number}"
     s.building = "#{Faker::Name.last_name}ビル"
     s.user_id = i + 1
