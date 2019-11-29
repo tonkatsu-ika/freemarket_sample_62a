9a30ee8d83801c6f62a13b68bd59ae8566bc0351
naoki <duvingtdeux@gmail.com>
2019-11-29 09:57:08 +0900

    indexのjsが競合して不具合あるのでコメントアウト

--- app/assets/javascripts/items/index.js
+++ app/assets/javascripts/items/index.js
@@ -28,13 +28,13 @@ $(document).on('turbolinks:load', function(){
     //    $('body,html').animate({scrollTop:position}, speed, 'swing');
     //    return false;
     // });
-    jQuery('a[href^=#]').click(function(){
-      var href= jQuery(this).attr("href");
-      var target = jQuery(href == "#" || href == "" ? 'body' : href);
-      var position = target.offset().top-headerHight;
-      jQuery("html, body").animate({scrollTop:position}, 550, "swing");
-      return false;
-    });
+    // jQuery('a[href^=#]').click(function(){
+    //   var href= jQuery(this).attr("href");
+    //   var target = jQuery(href == "#" || href == "" ? 'body' : href);
+    //   var position = target.offset().top-headerHight;
+    //   jQuery("html, body").animate({scrollTop:position}, 550, "swing");
+    //   return false;
+    // });
  
 
 });
