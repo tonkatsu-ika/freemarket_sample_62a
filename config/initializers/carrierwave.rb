1e2fbab8f9701ffb604f222c84e02c5d9241136c
naoki <duvingtdeux@gmail.com>
2019-12-03 12:48:57 +0900

    carrierwaveのinitirlizeのsyntax errorを修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,7 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage :fog
     config.fog_provider = 'fog/aws'
-    config_fog_directory = 'freemarket-t62a'
+    config.fog_directory = 'freemarket-t62a'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-t62a'
 
     config.fog_credentials = {
