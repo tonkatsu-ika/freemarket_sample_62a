5382bfda1d641a679c2c64e10749a58fd58122ee
naoki <duvingtdeux@gmail.com>
2019-12-03 09:26:35 +0900

    S3+Carrierwaveの初期設定（aws credentials含む)

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,10 +9,12 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config_fog_directory = 'freemarket-t62a'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-t62a'
+
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: '',
-      aws_secret_access_key: '',
+      aws_access_key_id: Rails.application.credentials.aws[:AWS_ACCESS_KEY_ID],
+      aws_secret_access_key: Rails.application.credentials.aws[:AWS_SECRET_ACCESS_KEY],
+      use_iam_profile: true,
       region: 'ap-northeast-1'
     }
 
