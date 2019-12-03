51b690c9b81f1b99dfd906b2352bcffdfd8715d5
naoki <duvingtdeux@gmail.com>
2019-12-03 09:11:58 +0900

    商品画像の投稿時にリサイズ(300x300)+Thumbnail作成

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -27,16 +27,16 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process scale: [200, 300]
+  process resize_to_fill: [300, 300, "Center"]
   #
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :thumb do
+    process resize_to_fill: [130, nil]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
