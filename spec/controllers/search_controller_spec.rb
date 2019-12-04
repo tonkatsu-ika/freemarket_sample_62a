8080576e2ea612aa0f84eeef7962981f529f38fd
naoki <duvingtdeux@gmail.com>
2019-12-04 11:31:40 +0900

    search controllerのテスト作成

---
+++ spec/controllers/search_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe SearchController, type: :controller do
+
+  describe "#index" do
+    # 正常なレスポンスか
+    it "responds successfully" do
+      get :index
+      expect(response).to be success
+    end
+    # 200レスポンスが返ってきているか
+    it "returns a 200 response" do
+      get :index
+      expect(response).to have_http_status "200"
+    end
+  end
+
+end
