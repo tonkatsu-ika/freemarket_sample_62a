f9eefef8a2c02d24b2563ac275fbe96cb1666deb
naoki <duvingtdeux@gmail.com>
2019-12-04 15:39:51 +0900

    ransack用のインスタンス変数をapplication controllerに定義

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,22 +3,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   :layout_by_resource
   
+  before_action :ransack_search, if: :use_ransack_search?
+
   protected
 
   def configure_permitted_parameters
     # devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :birthday, :first_name, :last_name, :first_name_katakana, :last_name_katakana,:telephone])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
-
-  ## ログアウト後に遷移するpathを設定
-  private
-  
-  # Overwriting the sign_out redirect path method
-  def after_sign_out_path_for(resource_or_scope)
-    root_path
-  end
-
-  protected
 
   def layout_by_resource
     if devise_controller?
@@ -27,4 +19,25 @@ class ApplicationController < ActionController::Base
       "application"
     end
   end
+
+  # 商品検索(items, search, mypageのみ適用)
+  def ransack_search
+    @item = Item.new
+    @q = Item.ransack(params[:q])
+    @items = @q.result(distinct: true)
+  end
+
+  private
+
+  ## ログアウト後に遷移するpathを設定  
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
+  # 商品検索用のbefore_action ransackサーチを使う場合はtrue(デフォはfalse)
+  def use_ransack_search?
+    false
+  end
+
 end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,13 @@
 class SearchController < ApplicationController
 
   def index
-    @item = Item.new
-    @q = Item.ransack(params[:q])
-    @items = @q.result(distinct: true)
+
   end
 
   private
-
+  # 商品検索用のbefore_action ransackサーチを使う場合はtrue(デフォはfalse)
+  def use_ransack_search?
+    true
+  end
+  
 end
