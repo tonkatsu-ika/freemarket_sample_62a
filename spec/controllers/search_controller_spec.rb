d7aaf3119fd52c8e0d9927cac6b9a2f42335b270
naoki <duvingtdeux@gmail.com>
2019-12-04 11:47:38 +0900

    controller testのsyntax error修正(現在0 failure)

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe SearchController, type: :controller do
     # 正常なレスポンスか
     it "responds successfully" do
       get :index
-      expect(response).to be success
+      expect(response).to be_success
     end
     # 200レスポンスが返ってきているか
     it "returns a 200 response" do
