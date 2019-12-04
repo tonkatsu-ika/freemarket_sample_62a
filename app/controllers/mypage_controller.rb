bb72c016ad88198d4ca45d770d09c6dc07c3f56a
naoki <duvingtdeux@gmail.com>
2019-12-04 17:07:43 +0900

    binding.pry削除

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -36,7 +36,6 @@ class MypageController < ApplicationController
   end
 
   def create
-    binding.pry
     Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       if params['payjp-token'].blank?
         redirect_to action: "card"
