360f8e15127effcf3a27b2bd968c5e5fc44c0d1d
yutaroyoshida <yoshida19950501@gmail.com>
2019-12-04 13:15:01 +0900

    itemsテーブルにlikes_countテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_071153) do
+ActiveRecord::Schema.define(version: 2019_12_04_041248) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "post_code", null: false
@@ -122,6 +122,7 @@ ActiveRecord::Schema.define(version: 2019_12_03_071153) do
     t.datetime "updated_at", null: false
     t.integer "prefecture_id"
     t.integer "transaction_status", default: 1
+    t.integer "likes_count"
     t.index ["brand_id"], name: "fk_rails_36708b3aa6"
     t.index ["category_id"], name: "fk_rails_89fb86dc8b"
     t.index ["days_before_ship_id"], name: "fk_rails_91824208cc"
