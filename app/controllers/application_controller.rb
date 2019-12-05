42fc4123e2b97a9c7e62c02bbe1d64ab628dde51
tomohiro1105 <paripari500@icloud.com>
2019-12-05 13:32:44 +0900

    ヘッダーブランドメニュー実装(jquery)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :get_category_all
+  before_action :get_brand_category_all
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
   :layout_by_resource
@@ -19,9 +19,9 @@ class ApplicationController < ActionController::Base
     root_path
   end
 
-  def get_category_all
+  def get_brand_category_all
+    @brand_all = Brand.all
     @category_all = Category.roots
-    # binding.pry
   end
 
   
