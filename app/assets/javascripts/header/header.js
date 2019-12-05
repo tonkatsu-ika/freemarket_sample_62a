0150d79072898faf4023829341b600940cb60e5d
tomohiro1105 <paripari500@icloud.com>
2019-12-05 16:31:03 +0900

    ヘッダーリスト調整

--- app/assets/javascripts/header/header.js
+++ app/assets/javascripts/header/header.js
@@ -1,144 +1,144 @@
-$(document).on('turbolinks:load', function(){
+// $(document).on('turbolinks:load', function(){
   
-  // ヘッダー マイページメニュー
+//   // ヘッダー マイページメニュー
   
-  $(".header-mypage").on("mouseover", function(){
-    $(".header__bottom__user-menu__li__mypage").show()
-  });
+//   $(".header-mypage").on("mouseover", function(){
+//     $(".header__bottom__user-menu__li__mypage").show()
+//   });
   
-  $(".header-mypage").on("mouseout", function(){
-    $(".header__bottom__user-menu__li__mypage").hide()
-  });
+//   $(".header-mypage").on("mouseout", function(){
+//     $(".header__bottom__user-menu__li__mypage").hide()
+//   });
 
-  $(".header__bottom__user-menu__li__mypage").on("mouseover", function(){
-    $(".header__bottom__user-menu__li__mypage").css({
-      'cursor': 'default'
-    });
-    $(".header__bottom__user-menu__li__mypage").show()
-  });
+//   $(".header__bottom__user-menu__li__mypage").on("mouseover", function(){
+//     $(".header__bottom__user-menu__li__mypage").css({
+//       'cursor': 'default'
+//     });
+//     $(".header__bottom__user-menu__li__mypage").show()
+//   });
 
-  $(".header__bottom__user-menu__li__mypage").on("mouseout", function(){
-    $(".header__bottom__user-menu__li__mypage").hide()
-  });
+//   $(".header__bottom__user-menu__li__mypage").on("mouseout", function(){
+//     $(".header__bottom__user-menu__li__mypage").hide()
+//   });
   
-  // ヘッダーやる事リスト
+//   // ヘッダーやる事リスト
   
-  $(".header-todo").on("mouseover", function(){
-    $(".header__bottom__user-menu__li__todo").show()
-  });
+//   $(".header-todo").on("mouseover", function(){
+//     $(".header__bottom__user-menu__li__todo").show()
+//   });
   
-  $(".header-todo").on("mouseout", function(){
-    $(".header__bottom__user-menu__li__todo").hide()
-  });
+//   $(".header-todo").on("mouseout", function(){
+//     $(".header__bottom__user-menu__li__todo").hide()
+//   });
 
-  $(".header__bottom__user-menu__li__todo").on("mouseover", function(){
-    $(".header__bottom__user-menu__li__todo").show()
-  });
+//   $(".header__bottom__user-menu__li__todo").on("mouseover", function(){
+//     $(".header__bottom__user-menu__li__todo").show()
+//   });
 
-  $(".header__bottom__user-menu__li__todo").on("mouseout", function(){
-    $(".header__bottom__user-menu__li__todo").hide()
-  });
+//   $(".header__bottom__user-menu__li__todo").on("mouseout", function(){
+//     $(".header__bottom__user-menu__li__todo").hide()
+//   });
 
-  // ヘッダーお知らせ
-  $(".header-notice").on("mouseover", function(){
-    $(".header__bottom__user-menu__li__notice").show()
-  });
+//   // ヘッダーお知らせ
+//   $(".header-notice").on("mouseover", function(){
+//     $(".header__bottom__user-menu__li__notice").show()
+//   });
   
-  $(".header-notice").on("mouseout", function(){
-    $(".header__bottom__user-menu__li__notice").hide()
-  });
+//   $(".header-notice").on("mouseout", function(){
+//     $(".header__bottom__user-menu__li__notice").hide()
+//   });
 
-  $(".header__bottom__user-menu__li__notice").on("mouseover", function(){
-    $(".header__bottom__user-menu__li__notice").show()
-  });
+//   $(".header__bottom__user-menu__li__notice").on("mouseover", function(){
+//     $(".header__bottom__user-menu__li__notice").show()
+//   });
 
-  $(".header__bottom__user-menu__li__notice").on("mouseout", function(){
-    $(".header__bottom__user-menu__li__notice").hide()
-  });
+//   $(".header__bottom__user-menu__li__notice").on("mouseout", function(){
+//     $(".header__bottom__user-menu__li__notice").hide()
+//   });
   
-  $(".header__bottom__user-menu__li__notice").on("mouseover", function(){
-    $(".header__bottom__user-menu__li__notice").css({
-      'background-color': '#fafafa'
-    });
-  });
+//   $(".header__bottom__user-menu__li__notice").on("mouseover", function(){
+//     $(".header__bottom__user-menu__li__notice").css({
+//       'background-color': '#fafafa'
+//     });
+//   });
 
-  $(".header__bottom__user-menu__li__notice").on("mouseout", function(){
-    $(".header__bottom__user-menu__li__notice").css({
-      'background-color': ''
-    });
-  });
+//   $(".header__bottom__user-menu__li__notice").on("mouseout", function(){
+//     $(".header__bottom__user-menu__li__notice").css({
+//       'background-color': ''
+//     });
+//   });
 
-//  ヘッダーカテゴリリスト
+// //  ヘッダーカテゴリリスト
 
-  $("#slide_categories").on("mouseover", function(){
-    $(".header-category-list").show()
-  });
+//   $("#slide_categories").on("mouseover", function(){
+//     $(".header-category-list").show()
+//   });
 
-  $("#slide_categories").on("mouseout", function(){
-    $(".header-category-list").hide()
-  });
+//   $("#slide_categories").on("mouseout", function(){
+//     $(".header-category-list").hide()
+//   });
 
-  $(".header-category-list").on("mouseover", function(){
-    $(".header-category-list").show()
-  });
+//   $(".header-category-list").on("mouseover", function(){
+//     $(".header-category-list").show()
+//   });
 
-  $(".header-category-list").on("mouseout", function(){
-    $(".header-category-list").hide()
-  });
+//   $(".header-category-list").on("mouseout", function(){
+//     $(".header-category-list").hide()
+//   });
 
-  $(".header-category-list-child").on("mouseover", function(){
-    $(".header-category-list").show()
-  });
+//   $(".header-category-list-child").on("mouseover", function(){
+//     $(".header-category-list").show()
+//   });
 
-  $(".header-category-list-child").on("mouseout", function(){
-    $(".header-category-list").hide()
-  });
+//   $(".header-category-list-child").on("mouseout", function(){
+//     $(".header-category-list").hide()
+//   });
 
 
-// ヘッダー子カテゴリリスト
+// // ヘッダー子カテゴリリスト
 
-  $(".header-category-list").on("mouseover", function(){
-    $(".header-category-list-child").show()
-  });
+//   $(".header-category-list").on("mouseover", function(){
+//     $(".header-category-list-child").show()
+//   });
 
-  $(".header-category-list").on("mouseout", function(){
-    $(".header-category-list-child").hide()
-  });
+//   $(".header-category-list").on("mouseout", function(){
+//     $(".header-category-list-child").hide()
+//   });
 
-  $(".header-category-list-child").on("mouseover", function(){
-    $(".header-category-list-child").show()
-  });
+//   $(".header-category-list-child").on("mouseover", function(){
+//     $(".header-category-list-child").show()
+//   });
 
-  $(".header-category-list-child").on("mouseout", function(){
-    $(".header-category-list-child").hide()
-  });
+//   $(".header-category-list-child").on("mouseout", function(){
+//     $(".header-category-list-child").hide()
+//   });
 
-// ヘッダー孫カテゴリリスト
+// // ヘッダー孫カテゴリリスト
   
-  $(".header-category-list-child").on("mouseover", function(){
-    $(".header-category-list-grandchild").show()
-  });
+//   $(".header-category-list-child").on("mouseover", function(){
+//     $(".header-category-list-grandchild").show()
+//   });
 
-  $(".header-category-list-child").on("mouseout", function(){
-    $(".header-category-list-grandchild").hide()
-  });
+//   $(".header-category-list-child").on("mouseout", function(){
+//     $(".header-category-list-grandchild").hide()
+//   });
 
-  $(".header-category-list-grandchild").on("mouseover", function(){
-    $(".header-category-list-grandchild").show()
-  });
+//   $(".header-category-list-grandchild").on("mouseover", function(){
+//     $(".header-category-list-grandchild").show()
+//   });
 
-  $(".header-category-list-grandchild").on("mouseout", function(){
-    $(".header-category-list-grandchild").hide()
-  });
+//   $(".header-category-list-grandchild").on("mouseout", function(){
+//     $(".header-category-list-grandchild").hide()
+//   });
 
-  $(".header-category-list-grandchild").on("mouseover", function(){
-    $(".header-category-list").show();
-    $(".header-category-list-child").show();
-  });
+//   $(".header-category-list-grandchild").on("mouseover", function(){
+//     $(".header-category-list").show();
+//     $(".header-category-list-child").show();
+//   });
 
-  $(".header-category-list-grandchild").on("mouseout", function(){
-    $(".header-category-list").hide();
-    $(".header-category-list-child").hide();
-  });
+//   $(".header-category-list-grandchild").on("mouseout", function(){
+//     $(".header-category-list").hide();
+//     $(".header-category-list-child").hide();
+//   });
 
-});
+// });
