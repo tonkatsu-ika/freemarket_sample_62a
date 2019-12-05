f96e4c640102d6b504d6ae77f70c0d448eef09e5
tomohiro1105 <paripari500@icloud.com>
2019-12-05 12:53:48 +0900

    ヘッダーカテゴリーリスト、非同期通信実装

--- app/assets/javascripts/header/category.js
+++ app/assets/javascripts/header/category.js
@@ -1,16 +1,22 @@
 $(document).on('turbolinks:load', function(){
   function buildChildHTML(category_children){
     var html = `<a class="header-category-list-child__content" id="${category_children.id}"
-                href="#">${category_children.category_name}</a>`
+                href="#">${category_children.category_name}</a>`;
     return html;
-  }
- ;
+  };
+
+  function buildGrandChildHTML(category_grandchildren){
+    var html =`<a class="header-category-list-grandchild__content" id="${category_grandchildren.id}"
+               href="#">${category_grandchildren.category_name}</a>`;
+    return html;
+  };
+ 
   $(".header-category-list__content").on("mouseover", function(){
     var id = this.id
     console.log(id)
     $(".header-category-list-child__content").remove();
-    $(".header-category-list-grandchild__content").remove();
     var parent_category = $(this).text()
+    console.log(parent_category)
     $.ajax({
       type: 'GET',
       url: '/items/get_category_children',
@@ -27,5 +33,35 @@ $(document).on('turbolinks:load', function(){
     });
     
   })
+  
+
+
+
+
+  
+
+  $(document).on("mouseover",".header-category-list-child__content", function(){
+    var id = this.id
+    console.log(id)
+    $(".header-category-list-grandchild__content").remove();
+    var parent_category = $(this).text()
+    console.log(parent_category)
+    $.ajax({
+      type: 'GET',
+      url: '/items/get_category_grandchildren',
+      data: { child_id: id },
+      dataType: 'json'
+    }).done(function(data){
+      data.forEach(function(category_grandchildren){
+        var html = buildGrandChildHTML(category_grandchildren);
+        $(".header-category-list-grandchild").append(html);
+      })
+
+    }).fail(function(){
+      console.log("失敗")
+    });
+    
+  })
+
 
 });
